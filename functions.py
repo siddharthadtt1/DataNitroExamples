e8a03862a7359472255b9a312f615169cd4d33db
siddharthadtt1 <siddharthadtt1@gmail.com>
2017-12-19 16:21:39 +0530

    Added DataNitro examples for Black-Scholes-Merton sample

--- functions.py
+++ functions.py
@@ -1,41 +1,57 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 18 22:22:17 2017
+Created on Tue Dec 19 12:36:13 2017
 
-@author: sid
+@author: sd835979
 """
 
-def bsm_put_option_value(S0, K, T, r, sigma):
+#
+# Valuation of Euorpean call options in BSM model
+# for use with DataNitro and Excel spreadsheets
+# functions.py
+#
+#
+
+# analytical Black-Scholes-Merton (BSM) formula 
+
+def bsm_call_option_value(S0, K, T, r, sigma):
     """
-        Valuation of European call option in BSM model
-        Analytical formula
-        
         Parameters
-        ===========
+        ============
         S0 : float
             initial stock/index level
         K : float
             strike price
         T : float
-            time to maturity (for t = 0)
+            time-to-maturity (for t=0)
         r : float
-            constant risk free short rate
+            constant risk-free short rate
         sigma : float
                 volatility factor in diffusion term
         
-        Returns 
+        Returns
         ========
-        value: float
-                present value of the European put option
+        value : float
+                present value of the European call option
     
     """
-    
     from math import log, exp, sqrt
-    from scipy import stats
+    import scipy.stats as stats
     
     S0 = float(S0)
-    d1 = (log(S0 / K)  + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-    d2 = (log(S0 / K)  + (r - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-    value = (S0 * stats.norm.cdf(d1, 0.0, 1.0) - 
+    d1 = (log(S0 / K) + ((r + (0.5 * sigma ** 2))/T))/ (sigma * sqrt(T))
+    d2 = (log(S0 / K) + ((r - (0.5 * sigma ** 2))/T))/ (sigma * sqrt(T))
+    
+    value = (S0 * stats.norm.cdf(d1, 0.0, 1.0)) - (
             K * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0))
-    return value
+    return value.round(2)
+    
+S0 = 100
+K = 100
+T = 1
+r = 0.05
+sigma = 0.2
+
+value = bsm_call_option_value(S0, K, T, r, sigma)
+print 'BSM put value option : ' + str(value)  
+
