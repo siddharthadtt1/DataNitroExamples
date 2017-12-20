f3a4606f6b19c3367ffc27481ac888110ce751a9
siddharthadtt1 <siddharthadtt1@gmail.com>
2017-12-20 15:20:01 +0530

    Added npv_sensitivity & chart examples

--- cash_flow_series.py
+++ cash_flow_series.py
@@ -5,7 +5,7 @@ Created on Tue Dec 19 18:29:17 2017
 @author: sd835979
 """
 
-class class_flow_series(object):
+class cash_flow_series(object):
     """
         Class to model a cash flow series
         
@@ -28,6 +28,8 @@ class class_flow_series(object):
             returns NPV for cash flow series
             
     """
+    import numpy as np
+    
     def __init__(self, name, cash_flows, time_list, short_rate):
         self.name = name
         self.cash_flows = cash_flows
@@ -41,6 +43,40 @@ class class_flow_series(object):
     def net_present_value(self):
         return np.sum(self.present_value_list())
     
-
+class cfs_sensitivity(cash_flow_series):
+    """
+        Class to net model cash flow for a range of short rates
         
+        Attributes
+        ===========
+        short_rates : list/array-like
+            corresponding list of short_rates
+        
+        Methods
+        =======
+        npv_sensitivity :
+            returns an array of npv for corresponding range of short rates
+        
+    """
+    import numpy as np
+    def npv_sensitivity(self, short_rates):
+        self.npvs = []
+        self.short_rates = short_rates
+        for rate in short_rates:
+            self.short_rate.rate = rate
+            self.npvs.append(self.net_present_value())
+        self.npvs = np.array(self.npvs)
+        return self.npvs
+            
+    
+    def display_chart(self, short_rates):
+        import matplotlib.pyplot as plt
+        self.npv_sensitivity(short_rates)
+        plt.plot(self.short_rates, self.npvs, 'b')
+        plt.plot(self.short_rates, self.npvs, 'ro')
+        plt.plot((0, max(self.short_rates)), (0, 0), 'r', lw=2)
+        plt.grid(True)
+        plt.xlabel(' short rate ')
+        plt.ylabel(' net present values ')
+                
         
